4a715287e9252ae4617c55c8f905f1e1ef6099f5
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2022-11-07 01:22:35 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -87,7 +87,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
+    r_driveCommand.execute();
   }
 
   @Override
