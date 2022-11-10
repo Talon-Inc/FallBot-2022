bc10309c0f45090242a1201378babc8526e8dcdd
Mike <110186790+MichealKing123@users.noreply.github.com>
2022-11-10 17:33:36 -0600

    Update PneumaticsSubsystem.java

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -4,17 +4,24 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticsSubsystem extends SubsystemBase {
   // Variables
+  private Compressor comp = null;
   private DoubleSolenoid sol1 = null;
+  private DoubleSolenoid sol2 = null;
+  private DoubleSolenoid sol3 = null;
 
   /** Creates a new ExampleSubsystem. */
   public PneumaticsSubsystem() {
+    comp = new Compressor(moduleType);
     sol1 = new DoubleSolenoid(PneumaticsModuleType.valueOf("CTREPCM"), 0, 1);
+    sol1 = new DoubleSolenoid(PneumaticsModuleType.valueOf("CTREPCM"), 2, 3);
+    sol1 = new DoubleSolenoid(PneumaticsModuleType.valueOf("CTREPCM"), 4, 5);
   }
 
   // Methods
