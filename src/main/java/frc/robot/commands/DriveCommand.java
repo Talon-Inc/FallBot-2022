fe7377a9536588dfe7993402074265d40a4f62b8
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2022-11-14 23:41:20 -0600

    Update DriveCommand.java

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /** An example command that uses an example subsystem. */
 public class DriveCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private boolean finished;
   private DriveTrain driveTrain;
   private Joystick joystick;
 
@@ -21,7 +20,6 @@ public class DriveCommand extends CommandBase {
    * @param driveTrain The subsystem used by this command.
    */
   public DriveCommand(DriveTrain driveTrain, Joystick joystick) {
-    this.finished = false;
     this.driveTrain = driveTrain;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
@@ -30,9 +28,7 @@ public class DriveCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    finished = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -91,12 +87,11 @@ public class DriveCommand extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     driveTrain.arcadeDrive(0, 0);
-    finished = true;
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return finished;
+    return false;
   }
 }
