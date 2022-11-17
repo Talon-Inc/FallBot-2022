67e1fd25983acb5c909e5f85dea517db10fedc81
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2022-11-17 17:39:16 -0600

    Create MouthOpen.java

--- src/main/java/frc/robot/commands/MouthOpen.java
+++ src/main/java/frc/robot/commands/MouthOpen.java
@@ -4,50 +4,28 @@
 
 package frc.robot.commands;
 
-import java.util.Random;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.PneumaticsSubsystem;
 
-public class TrickOrTreat extends CommandBase {
+public class MouthOpen extends CommandBase {
   private PneumaticsSubsystem pneumaticsSubsystem = null;
-  private boolean finished;
-  /** Creates a new TrickOrTreat. */
-  public TrickOrTreat(PneumaticsSubsystem pneumaticsSubsystem) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.pneumaticsSubsystem = pneumaticsSubsystem;
-    addRequirements(pneumaticsSubsystem);
 
-    finished = false;
+  /** Creates a new MouthOpen. */
+  public MouthOpen(PneumaticsSubsystem pneumaticsSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(pneumaticsSubsystem);
+    this.pneumaticsSubsystem = pneumaticsSubsystem;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    pneumaticsSubsystem.mouthOpen();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    // set the long seed value using Random constructor
-    Random random = new Random();
-
-    // flag == true
-    if (random.nextBoolean()) {
-      // true block, drop egg
-
-      pneumaticsSubsystem.pusherOpen();
-      // wait for some time
-      pneumaticsSubsystem.pusherClose();
-    } else {
-      // puff air
-      pneumaticsSubsystem.pufferOpen();
-      // wait 50 milliseconds
-      // wait(50, 0);
-      pneumaticsSubsystem.pufferClose();
-    }
-    
-    finished = true;
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
@@ -56,6 +34,6 @@ public class TrickOrTreat extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return finished;
+    return true;
   }
 }
