20a933dd08d9dacf7b4093fe702bde4177d1947b
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2022-11-17 17:55:27 -0600

    Update TrickOrTreat.java

--- src/main/java/frc/robot/commands/TrickOrTreat.java
+++ src/main/java/frc/robot/commands/TrickOrTreat.java
@@ -4,58 +4,23 @@
 
 package frc.robot.commands;
 
-import java.util.Random;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.PneumaticsSubsystem;
 
-public class TrickOrTreat extends CommandBase {
-  private PneumaticsSubsystem pneumaticsSubsystem = null;
-  private boolean finished;
-  /** Creates a new TrickOrTreat. */
-  public TrickOrTreat(PneumaticsSubsystem pneumaticsSubsystem) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.pneumaticsSubsystem = pneumaticsSubsystem;
-    addRequirements(pneumaticsSubsystem);
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TrickOrTreat extends InstantCommand {
+  private PneumaticsSubsystem pSubsystem = null;
 
-    finished = false;
+  public TrickOrTreat() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements();
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    // set the long seed value using Random constructor
-    Random random = new Random();
-
-    // flag == true
-    if (random.nextBoolean()) {
-      // true block, drop egg
-
-      pneumaticsSubsystem.pusherOpen();
-      // wait for some time
-      pneumaticsSubsystem.pusherClose();
-    } else {
-      // puff air
-      pneumaticsSubsystem.pufferOpen();
-      // wait 50 milliseconds
-      // wait(50, 0);
-      pneumaticsSubsystem.pufferClose();
-    }
+  public void initialize() {
     
-    finished = true;
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return finished;
   }
 }
