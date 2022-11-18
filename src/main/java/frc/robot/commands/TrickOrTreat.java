f816c08a5d391f0d07264db27a3b97c85f516fbc
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2022-11-17 18:25:42 -0600

    Update TrickOrTreat.java

--- src/main/java/frc/robot/commands/TrickOrTreat.java
+++ src/main/java/frc/robot/commands/TrickOrTreat.java
@@ -4,7 +4,11 @@
 
 package frc.robot.commands;
 
+import java.util.Random;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.PneumaticsSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -12,15 +16,37 @@ import frc.robot.subsystems.PneumaticsSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class TrickOrTreat extends InstantCommand {
   private PneumaticsSubsystem pSubsystem = null;
+  private Timer timer = null;
 
-  public TrickOrTreat() {
+  public TrickOrTreat(PneumaticsSubsystem pSubsystem) {
+    this.pSubsystem = pSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements();
+    addRequirements(pSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+
+    Random random = new Random();
+
+    //flag == true
+    if (random.nextBoolean()) {
+      //true block, drop egg
+
+      pSubsystem.pusherOpen();
+      WaitCommand wait = new WaitCommand(1); // in seconds
+      wait.initialize();
+      pSubsystem.pusherClose();
+    } else {
+      //puff air
+      pSubsystem.pufferOpen();
+      //wait 50 milliseconds
+      //wait(50,0)
+      // timer.wait(50);
+      WaitCommand wait = new WaitCommand(.05); // in seconds
+      wait.initialize();
+      pSubsystem.pufferClose();
+    }
   }
 }
